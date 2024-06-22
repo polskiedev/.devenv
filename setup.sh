3a3571d2c99fe4dc00e4ca5bb9cb7a72375f2aad
John Paul Fernandez <polskie.dev@gmail.com>
2024-06-22 18:03:32 +0800

    DEV-000003: Setup run

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ help() {
     echo "Usage: ./setup.sh [command]"
     echo
     echo "Commands:"
-    echo "  initialize     Run the initialize function"
+    echo "  first-run      Run after download"
+    echo "  init           Run the initialize function"
     echo "  update         Run the update function"
     echo "  help           Display this help message"
     echo "  make:links     Make symlinks for packages"
+    echo "  make:file      Generate source files for the package"
     echo
-    echo "If no command is provided, 'initialize' will be run by default."
+    echo "If no command is provided, 'help' will be run by default."
 }
 
 # Define the initialize function
@@ -35,52 +37,103 @@ make_symlinks() {
 	# list+=(package2)
 	# Loop through each element in the array
 	for element in "${list[@]}"; do
-		local target_dir="$HOME/$element"
-		local symlink_target="$script_dir/packages/$element"
+		local from="$HOME/$element"
+		local to="$script_dir/packages/$element"
 
-		__create_symlink "$target_dir" "$symlink_target"
+		__create_symlink "$from" "$to"
 	done
+
+    __create_symlink "$HOME/.devenv.sources.sh" "$PATH_DEVENV/.output/sources.sh"
 }
 
 # Define the function to check and create a symlink
 __create_symlink() {
-    local target_dir="$1"
-    local symlink_target="$2"
+    local from="$1"
+    local to="$2"
 
-    if [ -e "$target_dir" ]; then
-        if [ -L "$target_dir" ]; then
-            echo "$target_dir is already a symlink."
+    if [ -e "$from" ]; then
+        if [ -L "$from" ]; then
+            echo "$from is already a symlink."
         else
-            echo "Error: $target_dir already exists as a directory."
+            echo "Error: $from already exists as a directory."
         fi
     else
-        echo "$target_dir is not a symlink. Creating a symlink..."
-        ln -s "$symlink_target" "$target_dir"
-        echo "Symlink created from $target_dir to $symlink_target."
+        echo "$from is not a symlink. Creating a symlink..."
+        ln -s "$from" "$to"
+        echo "Symlink created from source '$from' to '$to'."
     fi
+}
+
+source .env/vars.sh
+
+makefile() {
+    local output_dir="$PATH_DEVENV/.output"
+    local output_file="$output_dir/sources.sh"
+
+    # Ensure the output directory exists
+    if [[ ! -d "$(realpath "$output_dir")" ]]; then
+        mkdir -p "$output_dir"
+        echo "Create: $output_dir directory"
+    fi
+
+	list=(".dotfiles" ".polskie.sh")
+
+    > "$output_file"  # Clear the file first
+
+    echo "#!/bin/bash" >> "$output_file"
+    echo "" >> "$output_file"
+    echo "if [ -z \"\$IS_SOURCED_DEVENV\" ]; then" >> "$output_file"
+    echo "  IS_SOURCED_DEVENV=true" >> "$output_file"
+    echo "  echo \"Loaded: .devenv/sources.sh\"" >> "$output_file"
+    echo "else" >> "$output_file"
+    echo "  echo \"Script '.devenv/sources.sh' already sourced.\"" >> "$output_file"
+    echo "  return 1" >> "$output_file"
+    echo "fi" >> "$output_file"
+    echo "" >> "$output_file"
+
+    local env_file="$PATH_DEVENV/.env/vars.sh"
+    echo "[[ ! -f \"$env_file\" ]] || source \"$env_file\"" >> "$output_file"
+    
+	# Loop through each element in the array
+	for element in "${list[@]}"; do
+        local source_file="$HOME/$element/.output/sources.sh"
+        echo "[[ ! -f \"$source_file\" ]] || source \"$source_file\"" >> "$output_file"
+	done
+}
+
+first_run() {
+    initialize
+    makefile
+    make_symlinks
 }
 
 # Check the parameter and call the corresponding function
 if [ -z "$1" ]; then
-    # No parameter passed, default to initialize
-    initialize
+    # No parameter passed, default to help
+    help
 else
     # Parameter passed, execute the corresponding function
     case "$1" in
-		help)
+		"help")
 			help
 			;;
-        init)
+        "init")
             initialize
+            ;;
+        "update")
+            update
+            ;;
+        "make:file")
+            makefile
             ;;
         "make:links")
             make_symlinks
             ;;
-        update)
-            update
+        "first-run")
+            first_run
             ;;
         *)
-            echo "Invalid parameter. Usage: ./setup.sh [init|update|"make:links"]"
+            echo "Invalid parameter. Usage: ./setup.sh ["first-run"|init|update|"make:links"|"make:file"]"
             exit 1
             ;;
     esac
