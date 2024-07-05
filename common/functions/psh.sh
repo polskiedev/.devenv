c97c6afb008e7797776c2910bc09c19279e55728
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-05 11:25:03 +0800

    DEV-000003: Add command alt

--- common/functions/psh.sh
+++ common/functions/psh.sh
@@ -6,6 +6,7 @@ psh() {
     local json_file="$PATH_POLSKIE_SH/config/commands.json"
 
     local real_command=$(jq -r --arg cmd "$command" '.commands[] | select(.command == $cmd) | .alias' "$json_file")
+    local real_alt_command=$(jq -r --arg cmd "$command" '.commands[] | select(.alt_command == $cmd) | .alias' "$json_file")
 
     # Check if alias_command is not empty
     if [[ -n "$real_command" ]]; then
@@ -13,6 +14,11 @@ psh() {
         echo "Command: $real_command"
         shift
         eval "$real_command $@"
+    elif [[ -n "$real_alt_command" ]]; then
+        # Execute the fetched command
+        echo "Alt Command: $real_alt_command"
+        shift
+        eval "$real_alt_command $@"
     elif [[ -z "$command" ]]; then
         cd "$HOME"
     else
