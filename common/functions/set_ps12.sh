a861e76570ff17e21621052f176b18449ec22e71
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-08 15:49:59 +0800

    DEV-000003: Clean up, fix set_ps

--- common/functions/set_ps12.sh
+++ common/functions/set_ps12.sh
@@ -2,43 +2,71 @@
 
 PROMPT_COMMAND="set_ps"
 
-ps1_repo_name() {
-	local repo_name
-	local msg=""
-    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-        repo_path=$(git rev-parse --show-toplevel)
-        repo_name=$(basename "$repo_path")
-		branch_name="$(git branch --show-current)"
+repository_git_info() {
+    declare -gA gitinfo=()
+    local repo_name
 
-        msg="$repo_name"
+    if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+        return 1
     fi
-	echo "$msg"
+    
+    repo_path=$(git rev-parse --show-toplevel)
+    repo_name=$(basename "$repo_path")
+
+    local branch_name="$(git branch --show-current)"
+    local ticket_no="$(echo "$branch_name" | awk -F/ '{print $NF}')"
+    local count=$(echo "$ticket_no" | grep -o "-" | wc -l)
+
+    if [ "$count" -gt 1 ]; then
+        ticket_no="$(echo "$ticket_no" | awk -F- '{print $1"-"$2}')"
+    fi
+
+    gitinfo["repository"]="$repo_name"
+    gitinfo["branch"]="$branch_name"
+    gitinfo["ticket_no"]="$ticket_no"
+    return 0
 }
 
-ps1_repo_branch_name() {
-	local branch_name="$(git branch --show-current)"
-	local ticket_no="$(echo "$branch_name" | awk -F/ '{print $NF}')"
-	local count=$(echo "$ticket_no" | grep -o "-" | wc -l)
-
-	if [ "$count" -gt 2 ]; then
-		ticket_no="$(echo "$ticket_no" | awk -F- '{print $1"-"$2}')"
-	fi
-	echo "$ticket_no"
+cleanup_repository_git_info() {
+	unset gitinfo
 }
 
 set_ps1() {
 	# echo "set_ps1()"
+	repository_git_info	
+
+	local is_repo=$?
+	local PS1_txt=""
+	local repository="${gitinfo["repository"]}"
+	local branch="${gitinfo["branch"]}"
+	local ticket_no="${gitinfo["ticket_no"]}"
+
+	cleanup_repository_git_info
+
+    # echo "Repository: ${gitinfo["repository"]}"
+    # echo "Branch: ${gitinfo["branch"]}"
+    # echo "Ticket No: ${gitinfo["ticket_no"]}"
 
 	local color1='\[\e[38;5;69m\]'
 	local color2='\[\e[38;5;76m\]'
 	local reset_stye="\[\e[0m\]"
-	local PS1_txt='\u@\h:\w'
 
-	PS1_txt+='\n'
-	PS1_txt+='\[\e[38;5;69m\]$(ps1_repo_name)\[\e[0m\]@\[\e[38;5;76m\]$(ps1_repo_branch_name)\[\e[0m\]'
-	PS1_txt+='✨'
+	PS1_txt='\u@\h:\w'
+
+    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+		PS1_txt+='\n'
+		PS1_txt+=$color1
+		# PS1_txt+="Repository: "
+		PS1_txt+=$repository
+		PS1_txt+=$reset_stye
+		PS1_txt+='@'
+		PS1_txt+=$color2
+		# PS1_txt+="Ticket No: "
+		PS1_txt+=$ticket_no
+		PS1_txt+=$reset_stye
+		PS1_txt+='✨'
+	fi
 	PS1_txt+='\$ '
-
 	PS1="$PS1_txt"
 }
 
