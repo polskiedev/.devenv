b6d8d7efe53459639abafdeec8e0cb93ed1eaa4a
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-09 08:44:54 +0800

    DEV-000003: Adjust get_datetime to accept days param

--- common/functions/datetime.sh
+++ common/functions/datetime.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 get_datetime() {
-	local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+	local datetime_format="%Y-%m-%d %H:%M:%S"
+	local timestamp=$(date +"$datetime_format")
+	local date_args=()
+
+    declare -A result
+    declare -a remaining_parameters
+    local requested_vars=("days")
+    local args=("$@")
+
+    process_args result remaining_parameters requested_vars[@] "${args[@]}"
+	if [[ "${result["days"]}" != false ]]; then
+		date_args+=("-d" "${result["days"]} days")
+	fi
+
 	if [[ -n "$1" ]]; then
 		case "$1" in
-			"--human")
-				timestamp=$(date +"%Y-%m-%d_%I%M%p")
-				;;
-			"--date")
-				timestamp=$(date +"%Y-%m-%d")
+			"--human") datetime_format="%Y-%m-%d_%I%M%p" ;;
+			"--date") datetime_format="%Y-%m-%d" ;;
+			"--backup") datetime_format="%Y%m%d_%I%M%p" ;;
+		esac
+
+		case "$1" in
+			"--human" | "--date")
+				date_args+=("+$datetime_format")
+				timestamp=$(date "${date_args[@]}")
 				;;
 			"--backup")
-				timestamp=$(date +"%Y%m%d_%I%M%p" | sed 's/AM/am/' | sed 's/PM/pm/')
+				date_args+=(+"$datetime_format")
+
+				timestamp=$(date "${date_args[@]}")
+				timestamp=$(echo $timestamp | sed 's/AM/am/' | sed 's/PM/pm/')
 				;;
 			*)
 				echo "Invalid parameter for get_datetime()"
