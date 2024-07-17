aa8ee464b6e868cde79b244d9e1454ba18e4683f
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-17 21:16:16 +0800

    DEV-000003: Add keybindings, issue in prompts

--- common/functions/keybindings.sh
+++ common/functions/keybindings.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Specifies the key sequence Ctrl-x Ctrl-s.
+# \C-u: Clears the current command line before running the script.
+# bind '"\C-x\C-s":"\C-u~/myscript.sh\n"'
+
 case $- in
     *i*) ;;
     *) return ;;
@@ -16,6 +20,10 @@ bind_command_ctrl_t() {
 	psh note:tomorrow
 }
 
+bind_command_ctrl_y() {
+	git_add_options_override_command_git
+}
+
 # Check if Alt+r is already bound and bind it if not
 if ! bind -p | grep -q '"\er"'; then
     bind -x '"\er": bind_command_alt_r'
@@ -27,4 +35,8 @@ fi
 
 if ! bind -p | grep -q '"\C-t"'; then
     bind -x '"\C-t": bind_command_ctrl_t'
+fi
+
+if ! bind -p | grep -q '"\C-y"'; then
+    bind -x '"\C-y":"bind_command_ctrl_y"'
 fi
